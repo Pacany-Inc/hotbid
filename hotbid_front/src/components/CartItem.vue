5e2f2f789296d2c8310a61483ebf5cb79f5ac3b7
hattorihanzooo <borntodie4@mail.ru>
2024-07-02 04:58:22 +1000

    Merge branch 'main' of https://github.com/Pacany-Inc/hotbid

---
+++ hotbid_front/src/components/CartItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <tr>
+    <td><router-link :to="item.product.get_absolute_url">{{ item.product.name }}</router-link></td>
+    <td>${{ item.product.price }}</td>
+    <td> {{ item.quantity }}</td>
+    <td>Заглушка</td>
+    <td><button class="delete"></button></td>
+  </tr>
+</template>
+
+<script>
+export default {
+  name: 'CartItem',
+  props: {
+    initialItem: Object
+  },
+  data() {
+    return {
+      item: this.initialItem
+    }
+  },
+  methods: {
+    },
+}
+</script>
